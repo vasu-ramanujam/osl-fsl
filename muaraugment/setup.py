b173a7b81a52c26fc55bae2ac3d3fa6d17ac24a6
Vasu Ramanujam <vasur@umich.edu>
2023-06-20 19:59:26 -0400

    manually added muar

---
+++ muaraugment/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+import pathlib
+
+HERE = pathlib.Path(__file__).parent
+README = (HERE / "README.md").read_text()
+
+setup(
+    name='MuarAugment',
+    version='1.1.1',
+    description='State-of-the-art data augmentation search algorithms in PyTorch',
+    long_description=README,
+    long_description_content_type="text/markdown",
+    author='Adam Mehdi',
+    author_email='amehdi.25@dartmouth.edu',
+    url='https://github.com/adam-mehdi/MuarAugment',
+    install_requires=['pytorch-lightning', 'kornia'],
+    packages=find_packages(exclude=("tests",))
+)
+

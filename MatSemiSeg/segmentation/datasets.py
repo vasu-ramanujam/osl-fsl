c542de762cbad64fdaf7e75a29aef75ec46cd99d
Vasu Ramanujam <vasur@umich.edu>
2023-07-07 11:05:39 -0400

    delete augs that didnt work in t2

--- MatSemiSeg/segmentation/datasets.py
+++ MatSemiSeg/segmentation/datasets.py
@@ -151,13 +151,13 @@ def get_transform(args, is_train):
             # initialize the transform list
             print(f"n_tfms: {N_TFMS}, magn: {MAGN}")
             transforms = [ A.Flip(p=1), 
-                      A.Rotate(MAGN*9, p=1),  
+                      #A.Rotate(MAGN*9, p=1),  
                       A.Posterize(num_bits=MAGN*8//30, p=1),
-                      A.Sharpen(alpha=(0.03 * MAGN, 0.03 * MAGN + .1), p=1),  
+                      #A.Sharpen(alpha=(0.03 * MAGN, 0.03 * MAGN + .1), p=1),  
                       A.RandomBrightnessContrast(brightness_limit=MAGN/30, contrast_limit=MAGN/30, p=1),
                       A.FancyPCA (alpha=MAGN/30, p=1),
-                      A.ShiftScaleRotate(shift_limit = MAGN/30, rotate_limit=MAGN*6, p=1),
-                      A.RandomToneCurve(scale=MAGN/30, p=1)
+                      A.ShiftScaleRotate(shift_limit = MAGN/30, rotate_limit=MAGN*6, p=1)
+                      #A.RandomToneCurve(scale=MAGN/30, p=1)
                      ]
             # randomly choose `N_TFMS` transforms from the list
             rng = np.random.default_rng()
